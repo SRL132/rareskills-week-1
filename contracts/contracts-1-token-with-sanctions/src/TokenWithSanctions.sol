07661706ba65f015965c3ee01afba5f60906e533
Sergi Roca Laguna <sergiroca@163.com>
2024-05-01 16:57:07 +0200

    minimum version

---
+++ contracts/contracts-1-token-with-sanctions/src/TokenWithSanctions.sol
@@ -0,0 +1,79 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.24;
+
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+error TokenWithSanctions__NotOwner();
+error TokenWithSanctions__SenderBanned();
+error TokenWithSanctions__RecipientBanned();
+
+/// @title Token that allows an admin to ban specified addresses from sending and receiving tokens.
+/// @author Sergi Roca Laguna
+/// @notice This token allows to ban addresses, which will not be able to send or receive tokens.
+/// @dev TODO
+contract TokenWithSanctions is ERC20 {
+    uint256 public constant TOKEN_SUPPLY = 500;
+    address public i_owner;
+    mapping(address => bool) public s_bannedAddresses;
+
+    modifier onlyOwner() {
+        if (msg.sender != i_owner) revert TokenWithSanctions__NotOwner();
+        _;
+    }
+
+    modifier senderNotBanned(address account) {
+        if (s_bannedAddresses[account])
+            revert TokenWithSanctions__SenderBanned();
+        _;
+    }
+
+    modifier recipientNotBanned(address account) {
+        if (s_bannedAddresses[account])
+            revert TokenWithSanctions__RecipientBanned();
+        _;
+    }
+
+    constructor(
+        address _owner,
+        string memory _name,
+        string memory _symbol
+    ) ERC20(_name, _symbol) {
+        i_owner = _owner;
+        _mint(_owner, TOKEN_SUPPLY);
+    }
+
+    function banAddress(address _address) external onlyOwner {
+        s_bannedAddresses[_address] = true;
+    }
+
+    function unbanAddress(address _address) external onlyOwner {
+        s_bannedAddresses[_address] = false;
+    }
+
+    function transfer(
+        address recipient,
+        uint256 amount
+    )
+        public
+        override
+        senderNotBanned(msg.sender)
+        recipientNotBanned(recipient)
+        returns (bool)
+    {
+        return super.transfer(recipient, amount);
+    }
+
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    )
+        public
+        override
+        senderNotBanned(sender)
+        recipientNotBanned(recipient)
+        returns (bool)
+    {
+        return super.transferFrom(sender, recipient, amount);
+    }
+}
---
+++ contracts/contracts-1-token-with-sanctions/test/TokenWithSanctions.t.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.24;
+
+import {Test, console} from "forge-std/Test.sol";
+import {TokenWithSanctions} from "../src/TokenWithSanctions.sol";
+
+contract TokenWithSanctionsTest is Test {
+    TokenWithSanctions public tokenWithSanctions;
+    address public owner = makeAddr("OWNER");
+    string public name = "TokenWithSanctions";
+    string public symbol = "TWS";
+    function setUp() public {
+        vm.prank(owner);
+        tokenWithSanctions = new TokenWithSanctions(owner, name, symbol);
+    }
+
+    function testOnlyOwnerCanBanAddress() public {
+        address bannedAddress = makeAddr("BANNED_ADDRESS");
+        vm.prank(owner);
+        tokenWithSanctions.banAddress(bannedAddress);
+        assertTrue(tokenWithSanctions.s_bannedAddresses(bannedAddress));
+        vm.expectRevert();
+        tokenWithSanctions.banAddress(bannedAddress);
+    }
+
+    function testOnlyOwnerCanUnbanAddress() public {
+        address bannedAddress = makeAddr("BANNED_ADDRESS");
+        vm.prank(owner);
+        tokenWithSanctions.banAddress(bannedAddress);
+        vm.expectRevert();
+        tokenWithSanctions.unbanAddress(bannedAddress);
+        vm.prank(owner);
+        tokenWithSanctions.unbanAddress(bannedAddress);
+        assertFalse(tokenWithSanctions.s_bannedAddresses(bannedAddress));
+    }
+
+    function testBannedSenderCannotSendTokens() public {
+        address bannedAddress = makeAddr("BANNED_ADDRESS");
+        vm.prank(owner);
+        tokenWithSanctions.banAddress(bannedAddress);
+        vm.expectRevert();
+        tokenWithSanctions.transfer(bannedAddress, 1);
+    }
+
+    function testBannedRecipientCannotBeTransferedTokens() public {
+        address bannedAddress = makeAddr("BANNED_ADDRESS");
+        vm.prank(owner);
+        tokenWithSanctions.banAddress(bannedAddress);
+        vm.expectRevert();
+        tokenWithSanctions.transferFrom(owner, bannedAddress, 1);
+    }
+}
